ec0ab432d275f63870fdef65f35cc375a29927fa
nayeembn008 <153630105+nayeembn008@users.noreply.github.com>
2024-09-19 13:50:12 +0600

    27-4

--- 27-4/js/home.js
+++ 27-4/js/home.js
@@ -1,1 +1,19 @@
-console.log("mew");
+// step 1: event handler of add money
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // step 2: get get money to be added to the account
+    const addMoneyInput = Number(
+      document.getElementById("input-add-money").value
+    );
+    console.log(addMoneyInput);
+
+    // step 3: get the number provided
+    const pinNumberInput = Number(
+      document.getElementById("input-pin-number").value
+    );
+    console.log(pinNumberInput);
+  });
